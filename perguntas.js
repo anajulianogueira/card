b7216ed75be606d22021da06f7b8d50cc2a8405f
anajulianogueira <00001088268717sp@al.educacao.sp.gov.br>
2024-11-25 19:03:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Raiz de 64?',
+    '8'
+)
+
+criaCartao(
+    'Matematica',
+    '2+5?',
+    '7'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é bacia hidrografica?',
+    ' área de terra delimitada por divisores de água, onde os cursos de água convergem para um único ponto de saída'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz vaca em Inglês?',
+    'Vaca em ingles é cow '
+)
